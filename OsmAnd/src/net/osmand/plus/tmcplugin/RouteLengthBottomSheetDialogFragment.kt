e379638520fb5ed7859788be384cb90640f56c0a
magasol <magdulinka.s@gmail.com>
2021-04-27 16:28:49 +0200

    read lon and lat in bottom sheet

--- OsmAnd/src/net/osmand/plus/tmcplugin/RouteLengthBottomSheetDialogFragment.kt
+++ OsmAnd/src/net/osmand/plus/tmcplugin/RouteLengthBottomSheetDialogFragment.kt
@@ -14,27 +14,29 @@ class RouteLengthBottomSheetDialogFragment: MenuBottomSheetDialogFragment() {
     }
 
     override fun createMenuItems(savedInstanceState: Bundle?) {
+        val longitude = arguments?.getDouble(LON_KEY, 0.0)
+        val latitude = arguments?.getDouble(LAT_KEY, 0.0)
         val title = resources.getString(R.string.plugin_tmc_choose_route)
         items.add(TitleItem(title))
 
         val shortRoute = SimpleBottomSheetItem.Builder()
                 .setTitle(getString(R.string.tmc_short_route_length))
                 .setLayoutId(R.layout.bottom_sheet_item_simple_right_icon)
-                .setOnClickListener { println("logger shortRoute") }
+                .setOnClickListener { println("logger shortRoute lon $longitude lat $latitude") }
                 .create()
         items.add(shortRoute)
 
         val mediumRoute = SimpleBottomSheetItem.Builder()
                 .setTitle(getString(R.string.tmc_medium_route_length))
                 .setLayoutId(R.layout.bottom_sheet_item_simple_right_icon)
-                .setOnClickListener { println("logger mediumRoute") }
+                .setOnClickListener { println("logger mediumRoute lon $longitude lat $latitude") }
                 .create()
         items.add(mediumRoute)
 
         val longRoute = SimpleBottomSheetItem.Builder()
                 .setTitle(getString(R.string.tmc_long_route_length))
                 .setLayoutId(R.layout.bottom_sheet_item_simple_right_icon)
-                .setOnClickListener { println("logger longRoute") }
+                .setOnClickListener { println("logger longRoute lon $longitude lat $latitude") }
                 .create()
         items.add(longRoute)
     }
